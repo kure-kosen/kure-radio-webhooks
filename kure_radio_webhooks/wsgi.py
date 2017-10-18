19197784103cdccf81f4cde0fb230e37544319e0
chanyou0311 <yuukun0311@gmail.com>
2017-10-19 00:47:03 +0900

    [fix]wsgi.py

--- kure_radio_webhooks/wsgi.py
+++ kure_radio_webhooks/wsgi.py
@@ -12,6 +12,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myDjango.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kure_radio_webhooks.settings")
 
 application = Cling(get_wsgi_application())
