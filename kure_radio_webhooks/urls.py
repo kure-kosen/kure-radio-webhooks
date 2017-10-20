2e3b6b02a036f656c395034ae8426cd7dc214e47
chanyou0311 <yuukun0311@gmail.com>
2017-10-20 13:12:27 +0900

    [add]trello urls.py

--- kure_radio_webhooks/urls.py
+++ kure_radio_webhooks/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^line/', include('line.urls')),
+    url(r'^trello/', include('trello.urls')),
     url(r'^admin/', admin.site.urls),
 ]
