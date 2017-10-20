1e96012346d5ca098c0bb6681d1b29554ac41d69
chanyou0311 <yuukun0311@gmail.com>
2017-10-20 13:12:16 +0900

    [add]trello/views.py

--- trello/views.py
+++ trello/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import json
+import requests
+import os
+
+REPLY_ENDPOINT = 'https://api.line.me/v2/bot/message/reply'
+ACCESS_TOKEN = os.getenv('LINE_ACCESS_TOKEN')
+HEADER = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + ACCESS_TOKEN
+}
+
+def index(request):
+    return HttpResponse("Hello World")
+
+def callback(request):
+    return HttpResponse("callback")
+
